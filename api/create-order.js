a1a82cf3e2ae969c33225b8f1787bc3ee02d3556
msummer91 <m.sommer1991@gmail.com>
2026-08-12 14:50:42 +0200

    Make Prodigi endpoint switchable via PRODIGI_SANDBOX env var

--- api/create-order.js
+++ api/create-order.js
@@ -62,7 +62,10 @@ module.exports = async function handler(req, res) {
   };
 
   try {
-    const response = await fetch('https://api.prodigi.com/v4.0/orders', {
+    const prodigiBase = process.env.PRODIGI_SANDBOX === 'true'
+      ? 'https://api.sandbox.prodigi.com/v4.0'
+      : 'https://api.prodigi.com/v4.0';
+    const response = await fetch(`${prodigiBase}/orders`, {
       method:  'POST',
       headers: {
         'Content-Type': 'application/json',
